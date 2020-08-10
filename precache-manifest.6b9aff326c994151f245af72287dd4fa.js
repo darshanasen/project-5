26b8b5e2fc624712272b24ba33f21dce7028fb7d
Darshana Sen <darsh.sen@gmail.com>
2020-08-10 17:33:17 -0300

    Updates

---
+++ precache-manifest.6b9aff326c994151f245af72287dd4fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64901eb10eace3b9894fe21451a14680",
+    "url": "/project-5/index.html"
+  },
+  {
+    "revision": "6acb6855b232e8fc093f",
+    "url": "/project-5/static/css/main.66d05e20.chunk.css"
+  },
+  {
+    "revision": "9b4ed2c2180fb64a7736",
+    "url": "/project-5/static/js/2.df216853.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/project-5/static/js/2.df216853.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6acb6855b232e8fc093f",
+    "url": "/project-5/static/js/main.b905b4a2.chunk.js"
+  },
+  {
+    "revision": "11308174d8a73326eec5",
+    "url": "/project-5/static/js/runtime-main.c32d31c6.js"
+  }
+]);
